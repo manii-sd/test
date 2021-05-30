acea084d1fba7420d5a78b262946d80c955850e4
manii <manii.shahdoust@gmail.com>
2021-05-31 02:28:51 +0430

    add: start of using redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import { Provider } from 'react-redux'
+// import configureStore from './store/configureStore'
 
 import {Screenone} from './screen1';
 
-
-
+// const store = configureStore()
 
 ReactDOM.render(<Screenone/>, document.getElementById('root'),);
