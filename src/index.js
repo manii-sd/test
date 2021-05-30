da00923a4808e59d930ea063d941fe65e2d5ec23
manii <manii.shahdoust@gmail.com>
2021-05-31 01:27:32 +0430

    ititial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Screenone} from './screen1';
+
+
+
+
+ReactDOM.render(<Screenone/>, document.getElementById('root'),);
---
+++ src/screen1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+
+export const Screenone =() =>{
+
+
+  return<> 
+    <button>BOOM</button>
+    <br/>
+    <input type="checkbox" onChecked={false}></input>
+
+  </>
+}
+
+export default Screenone;
