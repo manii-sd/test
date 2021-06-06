c18951fe44a18c83faafe20828e6e056d0dbadf9
manii <manii.shahdoust@gmail.com>
2021-06-06 13:31:47 +0430

    more code +  errors

--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,4 +1,4 @@
-//import * as ActionTypes from '../constants/ActionTypes'
+import * as ActionTypes from '../../constants/ActionTypes'
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -21,41 +21,12 @@ const initialState = [ ]
 
 const todos = (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.FETCH_TODOS_SUCCEEDED:
-      return action.todos.map(t =>
-          todo(t, action)
-      )
-    case ActionTypes.ADD_TODO_SUCCEEDED:
+    case ActionTypes.CLICK_BUTTON_WITH_CHECKOX:
+      return 
+      case ActionTypes.CLICK_BUTTON_WITHOUT_CHECKOX:
       return [
-        ...state,
-        todo(state, action)
       ]
-    case ActionTypes.DELETE_TODO_REQUESTED:
-      return state.filter(todo =>
-        todo.id !== action.id
-      )
-    case ActionTypes.EDIT_TODO_REQUESTED:
-      return state.map(t =>
-        t.id === action.id ?
-          todo(t, action) :
-          t
-      )
-    case ActionTypes.COMPLETE_TODO_REQUESTED:
-      return state.map(t =>
-        t.id === action.id ?
-          todo(t, action) :
-          t
-      )
-    case ActionTypes.CLEAR_COMPLETED_REQUESTED:
-      return state.filter(todo => !todo.completed)
-    case ActionTypes.COMPLETE_ALL_REQUESTED:
-      const areAllMarked = state.every(todo => todo.completed)
-      return state.map(t => 
-        todo(
-          t, 
-          { type: action.type, completed: !areAllMarked }
-        )
-      )
+    
     default:
       return state
   }
